6e8d32acd645cf1d624d894cf9d0c5332ba72ec7
kiransamjhana <kiranmanandhar956@gmail.com>
2023-09-09 14:52:21 +1000

    new file

---
+++ server.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import connectMongoDB from "./src/config/mongoconfig.js";
+connectMongoDB();
+
+const PORT = process.env.PORT || 8200;
+const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "sucess",
+    message: "thank you very much yo9ur sever is very fast",
+  });
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const code = error.statusCode || 500;
+  res.status(code).json({
+    status: "error",
+    message: error.message,
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`your server is running at http://localhost:${PORT}`);
+});
